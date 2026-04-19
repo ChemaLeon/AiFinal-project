58e1d8276254f2acf43ae044fa5ea698ccd98506
LoadingScreen <milkbubbletea92620@gmail.com>
2026-04-19 22:26:09 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,78 @@
 import streamlit as st
-import openai
+from openai import OpenAI
+import json
+
+if 'ingredients' not in st.session_state:
+    st.session_state["ingredients"] = []
+
+
+client = OpenAI(
+    api_key=st.secrets["api_key"]
+)
 
 
 col1, col2, col3 = st.columns(3)
 with col2:
-    """
-    # CookSmart
 
-    Cook Smarter, Not Harder
-    
-    """
-    st.title("CookSmart")
+    st.header("CookSmart")
     st.write("Cook Smarter, Not Harder")
+
+
+system_prompt = """
+You are a cooking assistant.
+
+Return ONLY valid JSON in this format:
+
+{
+  "recipes": [
+    {
+      "title": "",
+      "ingredients": [],
+      "steps": []
+    }
+  ]
+}
+
+Rules:
+- Use only given ingredients + basic pantry items
+- Dont use other ingredients, only the ones the person gave.
+- Must be valid recipes, not some made up ones. Google them off the internet
+- Generate exactly 5 recipes
+- Max 10 steps per recipe
+"""
+
+
+
+if 'chat' not in st.session_state:
+    st.session_state["chat"] = {'role':'system','content':system_prompt}
+
+with st.form('hp_form'):
+    ingredients = st.text_input("What are the ingredients you have?(Write commas between each induvidual ingredient, atleast 4 ingredients) ")
+    submit_button = st.form_submit_button("Submit ingredients")
+    user_prompt = f"""
+These are the ingredients I have:
+{ingredients}
+
+What can I cook?
+"""
+
+    if submit_button:
+        st.write('ingredients taken')
+
+        st.session_state['chat'].append({'role':'user','content':user_prompt})
+        if 'ingredients' in st.session_state:
+            st.session_state['ingredients'].append(ingredients)
+
+
+with st.form("get-recipes"):
+    st.write("Press to get recipe")
+    get_recipe = st.form_submit_button('Get recipe list made with ingredients!')
+    if get_recipe:
+    
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            response_format= {'type':'json_object'},
+            messages=st.session_state['chat_history'],
+        
+            )
+        st.write(json.loads(response.choices[0].message.content))
