eadb30aca4b410cf441ca70f797b189a1cb38b7e
LoadingScreen <milkbubbletea92620@gmail.com>
2026-04-19 21:44:18 +0000

    couloums

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 import openai
 
 st.title("CookSmart", text_alignment="center")
-st.write("Cook Smarter, not Harder. Recipes straight from what you have.")
+
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.write("Cook Smarter Not Harder ")
