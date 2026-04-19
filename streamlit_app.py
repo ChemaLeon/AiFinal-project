66556260e65c683b48c472328e3e608c16950e6d
LoadingScreen <milkbubbletea92620@gmail.com>
2026-04-19 21:55:25 +0000

    html

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import openai
 
 col1, col2, col3 = st.columns(3)
 with col2:
-    st.markdown("<h1>CookSmart</h1><br><h2>Cook smarter not harder></h2>")
+    st.html("<h1>CookSmart</h1><br><h2>Cook smarter not harder></h2>")
