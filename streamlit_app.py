c362a9edd8de42d53517888047c77d396bc21e0a
LoadingScreen <milkbubbletea92620@gmail.com>
2026-04-19 21:44:56 +0000

    more comloms

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import openai
 
-st.title("CookSmart", text_alignment="center")
 
 col1, col2, col3 = st.columns(3)
 with col2:
+    st.header("CookSmart")
     st.write("Cook Smarter Not Harder ")
